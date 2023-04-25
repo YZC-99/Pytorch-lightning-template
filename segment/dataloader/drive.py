86e30129cadda04251fe5fe6cd8b20ba9984e6fa
yzc <1455460787@qq.com>
2023-04-25 15:25:52 +0800

    初次尝试

---
+++ segment/dataloader/drive.py
@@ -0,0 +1,120 @@
+import os
+import numpy as np
+import cv2
+import albumentations
+import glob
+import random
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+def preprocess_mask(img):
+    mask = np.zeros_like(img)
+    mask[img >= 150] = 1
+    return mask
+
+
+class SegmentationBase(Dataset):
+    def __init__(self,
+                 data_csv, data_root, segmentation_root,
+                 size=None, random_crop=False, interpolation="bicubic",
+                 n_labels=2, shift_segmentation=False,
+                 ):
+        self.n_labels = n_labels
+        self.shift_segmentation = shift_segmentation
+        self.data_csv = data_csv
+        self.data_root = data_root
+        self.segmentation_root = segmentation_root
+        with open(self.data_csv, "r") as f:
+            self.image_paths = f.read().splitlines()
+        self._length = len(self.image_paths)
+        self.labels = {
+            "relative_file_path_": [l for l in self.image_paths],
+            "file_path_": [os.path.join(self.data_root, l)
+                           for l in self.image_paths],
+            "segmentation_path_": [os.path.join(self.segmentation_root, l).replace("tif","gif")
+                                   for l in self.image_paths]
+        }
+
+        size = None if size is not None and size<=0 else size
+        self.size = size
+        if self.size is not None:
+            self.interpolation = interpolation
+            self.interpolation = {
+                "nearest": cv2.INTER_NEAREST,
+                "bilinear": cv2.INTER_LINEAR,
+                "bicubic": cv2.INTER_CUBIC,
+                "area": cv2.INTER_AREA,
+                "lanczos": cv2.INTER_LANCZOS4}[self.interpolation]
+            self.image_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
+                                                                 interpolation=self.interpolation)
+            self.segmentation_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
+                                                                        interpolation=cv2.INTER_NEAREST)
+            self.center_crop = not random_crop
+            if self.center_crop:
+                self.cropper = albumentations.CenterCrop(height=self.size, width=self.size)
+            else:
+                self.cropper = albumentations.RandomCrop(height=self.size, width=self.size)
+            self.preprocessor = self.cropper
+
+    def __len__(self):
+        return self._length
+
+    def __getitem__(self, i):
+        example = dict((k, self.labels[k][i]) for k in self.labels)
+        image = Image.open(example["file_path_"])
+        if not image.mode == "RGB":
+            image = image.convert("RGB")
+        image = np.array(image).astype(np.uint8)
+        if self.size is not None:
+            image = self.image_rescaler(image=image)["image"]
+        segmentation = Image.open(example["segmentation_path_"])
+        if not segmentation.mode == "L":
+            segmentation = segmentation.convert("L")
+        assert segmentation.mode == "L", segmentation.mode
+        segmentation = np.array(segmentation).astype(np.uint8)
+
+        # Preprocess
+        segmentation = preprocess_mask(segmentation)
+
+        if self.shift_segmentation:
+            # used to support segmentations containing unlabeled==255 label
+            segmentation = segmentation+1
+        if self.size is not None:
+            segmentation = self.segmentation_rescaler(image=segmentation)["image"]
+        if self.size is not None:
+            processed = self.preprocessor(image=image,
+                                          mask=segmentation
+                                          )
+        else:
+            processed = {"image": image,
+                         "mask": segmentation
+                         }
+
+        example["image"] = (processed["image"]/127.5 - 1.0).astype(np.float32)
+        example["image"] = np.transpose(example["image"],(2, 0, 1))
+
+        segmentation = processed["mask"]
+        onehot = np.eye(self.n_labels)[segmentation]
+        example["label"] = np.transpose(onehot,(2, 0, 1))
+        return example
+
+
+class DRIVESegTrain(SegmentationBase):
+    def __init__(self, size=None, random_crop=False, interpolation="bicubic"):
+        super().__init__(data_csv='data/DRIVE/drive_train.txt',
+                         data_root='data/DRIVE/images',
+                         segmentation_root='data/DRIVE/masks',
+                         size=size, random_crop=random_crop, interpolation=interpolation,
+                         n_labels=2)
+
+
+class DRIVESegEval(SegmentationBase):
+    def __init__(self, size=None, random_crop=False, interpolation="bicubic"):
+        super().__init__(data_csv='data/DRIVE/drive_eval.txt',
+                         data_root='data/DRIVE/images',
+                         segmentation_root='data/DRIVE/masks',
+                         size=size, random_crop=random_crop, interpolation=interpolation,
+                         n_labels=2)
+
+
---
+++ segment/modules/unet.py
@@ -0,0 +1,202 @@
+from typing import Dict
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from typing import List,Tuple, Dict, Any, Optional
+from omegaconf import OmegaConf
+from segment.utils.general import initialize_from_config
+from torch.optim import lr_scheduler
+import pytorch_lightning as pl
+
+class DoubleConv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        if mid_channels is None:
+            mid_channels = out_channels
+        super(DoubleConv, self).__init__(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+
+class Down(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__(
+            nn.MaxPool2d(2, stride=2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        x1 = self.up(x1)
+        # [N, C, H, W]
+        diff_y = x2.size()[2] - x1.size()[2]
+        diff_x = x2.size()[3] - x1.size()[3]
+
+        # padding_left, padding_right, padding_top, padding_bottom
+        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
+                        diff_y // 2, diff_y - diff_y // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class OutConv(nn.Sequential):
+    def __init__(self, in_channels, num_classes):
+        super(OutConv, self).__init__(
+            nn.Conv2d(in_channels, num_classes, kernel_size=1)
+        )
+
+
+class UNet(pl.LightningModule):
+    def __init__(self,
+                 image_key: str,
+                 in_channels: int,
+                 num_classes: int,
+                 bilinear: bool,
+                 base_c: int,
+                 loss: OmegaConf,
+                 scheduler: Optional[OmegaConf] = None,
+                 ):
+        super(UNet, self).__init__()
+        self.image_key = image_key
+        self.loss = initialize_from_config(loss)
+        self.scheduler = scheduler
+
+
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.bilinear = bilinear
+
+        self.in_conv = DoubleConv(in_channels, base_c)
+        self.down1 = Down(base_c, base_c * 2)
+        self.down2 = Down(base_c * 2, base_c * 4)
+        self.down3 = Down(base_c * 4, base_c * 8)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(base_c * 8, base_c * 16 // factor)
+        self.up1 = Up(base_c * 16, base_c * 8 // factor, bilinear)
+        self.up2 = Up(base_c * 8, base_c * 4 // factor, bilinear)
+        self.up3 = Up(base_c * 4, base_c * 2 // factor, bilinear)
+        self.up4 = Up(base_c * 2, base_c, bilinear)
+        self.out_conv = OutConv(base_c, num_classes)
+
+        # self.Encoder = nn.Sequential(
+        #     # self.down1,
+        #     # self.down2,
+        #     # self.down3,
+        #     # self.down4,
+        # Down(base_c, base_c * 2),
+        # Down(base_c * 2, base_c * 4),
+        # Down(base_c * 4, base_c * 8),
+        # Down(base_c * 8, base_c * 16 // factor)
+        # )
+        # self.Decoder = nn.Sequential(
+        #     # self.up1,
+        #     # self.up2,
+        #     # self.up3,
+        #     # self.up4,
+        #     Up(base_c * 16, base_c * 8 // factor, bilinear),
+        #     Up(base_c * 8, base_c * 4 // factor, bilinear),
+        #     Up(base_c * 4, base_c * 2 // factor, bilinear),
+        #     Up(base_c * 2, base_c, bilinear),
+        # )
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        x1 = self.in_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.out_conv(x)
+        return logits
+
+    def init_from_ckpt(self,path: str,ignore_keys: List[str] = list()):
+        sd = torch.load(path,map_location='cpu')['state_dict']
+        keys = list(sd.keys())
+        for k in keys:
+            for ik in ignore_keys:
+                if k.startswith(ik):
+                    print("Deleting key {} from state_dict.".format(k))
+                    del sd[k]
+        self.load_state_dict(sd, strict=False)
+        print(f"Restored from {path}")
+
+    def get_input(self, batch: Tuple[Any, Any], key: str = 'image') -> Any:
+        x = batch[key]
+        if len(x.shape) == 3:
+            x = x[..., None]
+        if x.dtype == torch.double:
+            x = x.float()
+        return x.contiguous()
+
+    def training_step(self, batch: Tuple[Any, Any], batch_idx: int, optimizer_idx: int = 0) -> torch.FloatTensor:
+        x = self.get_input(batch, self.image_key)
+        y = batch['label']
+        logits = self(x)
+
+        loss = self.loss(logits, y)
+
+        self.log("train/total_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch: Tuple[Any, Any], batch_idx: int) -> Dict:
+        x = self.get_input(batch, self.image_key)
+        y = batch['label']
+        logits = self(x)
+        loss = self.loss(logits, y)
+        self.log("val/loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
+        return loss
+
+    def configure_optimizers(self) -> Tuple[List, List]:
+        lr = self.learning_rate
+        optim_groups = list(self.in_conv.parameters()) + \
+                       list(self.down1.parameters()) + \
+                       list(self.down2.parameters()) + \
+                       list(self.down3.parameters()) + \
+                       list(self.down4.parameters()) + \
+                       list(self.up1.parameters()) + \
+                       list(self.up2.parameters()) + \
+                       list(self.up3.parameters()) + \
+                       list(self.up4.parameters()) + \
+                       list(self.out_conv.parameters())
+
+        optimizers = [torch.optim.AdamW(optim_groups, lr=lr, betas=(0.9, 0.99), weight_decay=1e-4)]
+        schedulers = []
+
+        if self.scheduler is not None:
+            self.scheduler.params.start = lr
+            scheduler = initialize_from_config(self.scheduler)
+
+            schedulers = [
+                {
+                    'scheduler': lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler.schedule),
+                    'interval': 'step',
+                    'frequency': 1
+                } for optimizer in optimizers
+            ]
+
+        return optimizers, schedulers
+
+    def log_images(self, batch: Tuple[Any, Any], *args, **kwargs) -> Dict:
+        log = dict()
+        x = self.get_input(batch, self.image_key).to(self.device)
+        log["originals"] = x
+        log["predict"] = self(x)
+        return log
