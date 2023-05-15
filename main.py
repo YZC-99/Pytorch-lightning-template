164a393456b31c9c7ac6f58ae108b484c1e552e5
yzc <yzc@qq.com>
2023-05-15 21:39:43 +0800

    增加VQGAN的代码

--- main.py
+++ main.py
@@ -6,16 +6,30 @@
 
 import os
 import sys
-import argparse
+import argparse, os, sys, datetime, glob, importlib
 from pathlib import Path
 from omegaconf import OmegaConf
 import pytorch_lightning as pl
 
 from segment.utils.general import get_config_from_file, initialize_from_config, setup_callbacks
 
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, default='idrid_dr_single_res50FCN')
+    parser.add_argument('-c', '--config', type=str, default='idrid_vqgan')
     parser.add_argument('-s', '--seed', type=int, default=0)
     parser.add_argument('-nn', '--num_nodes', type=int, default=1)
     parser.add_argument('-ng', '--num_gpus', type=int, default=1)
