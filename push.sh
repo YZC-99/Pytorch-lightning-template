6d9f107d370a48f0a244bff46bab88bda80901d0
yzc <yzc@qq.com>
2023-05-18 09:30:13 +0800

    first commit

--- push.sh
+++ push.sh
@@ -16,7 +16,7 @@ git commit -m "Update .gitignore"
 
 
 echo "Setting up access token..."
-TOKEN="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"
+TOKEN="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"
 
 echo "Building remote repository URL..."
 REPO_URL="https://$TOKEN@github.com/YZC-99/Pytorch-lightning-template.git"
