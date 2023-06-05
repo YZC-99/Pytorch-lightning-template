48c2199d194af573870a19c943eab8a6fc56bfe9
yzc <yzc@qq.com>
2023-06-05 21:18:37 +0800

    添加了cam

--- scripts/cam.py
+++ scripts/cam.py
@@ -14,32 +14,20 @@ def hook(module, input, output):
 
 # 定义模型
 model = resnet50(pretrained=True).eval()
-# num_ftrs = model.fc.in_features
-# model.fc = torch.nn.Linear(num_ftrs, 2)
-# model = smp.Unet(classes=2).eval()
-# print(model)
-# layer4 = model.segmentation_head
-# hook_handle = layer4.register_forward_hook(hook)
+# chpt_path = ''
+# sd = torch.load(chpt_path, map_location='cpu')['state_dict']
+# keys = list(sd.keys())
+# model.load_state_dict(sd, strict=False)
 
-# model = seg.fcn_resnet50(pretrained=True)
-# 设置CAM extractor
 cam_extractor = GradCAMpp(model,target_layer='layer4')
 # cam_extractor = SmoothGradCAMpp(model,target_layer='decoder')
 
 # 读取图片
-img = read_image("../data/cat/2007_002597.jpg")
-input_tensor = normalize(resize(img, (224, 224)) / 255., [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+img = read_image("../data/cat/T0001.jpg")
+input_tensor = normalize(resize(img, (128, 128)) / 255., [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
 # 获得模型的输出
 out = model(input_tensor.unsqueeze(0))
-# print(model)
-# print(out.shape)
-# print(out.argmax(dim=-1))
-# plt.imshow(out[0,0,...].squeeze(0).detach().numpy()); plt.axis('off'); plt.tight_layout(); plt.show()
-# plt.imshow(out[0,1,...].squeeze(0).detach().numpy()); plt.axis('off'); plt.tight_layout(); plt.show()
-# # 叠加可视化的图
-# result = overlay_mask(to_pil_image(img), to_pil_image(out[0,1,...].detach().numpy().squeeze(0), mode='F'), alpha=0.5)
-# hook_handle.remove()
 
 # 将模型的输出输入到cam_extractor
 activation_map = cam_extractor(out.squeeze(0).argmax().item(),out)
